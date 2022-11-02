ed08d17934527d9bb2552e84ad73ab55af29e1b6
David Klimavich <dklimavich@gmail.com>
2022-11-02 08:53:15 -0400

    split aliases into seperate file

--- bootstrap_mint.sh
+++ bootstrap_mint.sh
@@ -1,11 +1,30 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+LASTPASS_EMAIL=$1
+
+
+# update 
 sudo add-apt-repository ppa:mozillacorp/mozillavpn
 sudo apt update
 sudo apt upgrade -y
-sudo apt install vim steam python3 git openssh-server virtualbox mumble dolphin bashtop resolvconf mozillavpn ssh xclip
+
+# apt installs
+sudo apt install vim steam python3 git openssh-server virtualbox mumble dolphin bashtop resolvconf mozillavpn ssh xclip lastpass-cli
+
+# snap installs
 sudo apt install snapd
 snap install discord
 snap install spotify
 snap install mc-installer
 
-echo "alias xc='xclip -sel c'" >> ~/.bashrc
+# Aliases
+cat ${SCRIPT_DIR}/aliases >> ~/.bashrc
+
+# Open apps to start updates
+steam &
+spotify &
+discord &
+
+lpass login ${LASTPASS_EMAIL}
 
