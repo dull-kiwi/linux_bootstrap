39555d8b7d9524de58d6ae6dfdfe17893badf8bd
David Klimavich <dklimavich@gmail.com>
2022-11-01 22:57:06 -0400

    add mint bootstrap script

---
+++ bootstrap_mint.sh
@@ -0,0 +1,11 @@
+sudo add-apt-repository ppa:mozillacorp/mozillavpn
+sudo apt update
+sudo apt upgrade -y
+sudo apt install vim steam python3 git openssh-server virtualbox mumble dolphin bashtop resolvconf mozillavpn ssh xclip
+sudo apt install snapd
+snap install discord
+snap install spotify
+snap install mc-installer
+
+echo "alias xc='xclip -sel c'" >> ~/.bashrc
+
