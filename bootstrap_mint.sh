549baef2105275dbcff9965dacd93b617ffb13c3
David Klimavich <dklimavich@gmail.com>
2023-04-19 14:25:38 -0400

    Added a few packages

--- bootstrap_mint.sh
+++ bootstrap_mint.sh
@@ -1,7 +1,27 @@
 sudo add-apt-repository ppa:mozillacorp/mozillavpn
 sudo apt update
 sudo apt upgrade -y
-sudo apt install vim steam python3 git openssh-server virtualbox mumble dolphin bashtop resolvconf mozillavpn ssh xclip
+sudo apt install 
+vim                  \ 
+steam                \
+python3              \
+git                  \
+openssh-server       \
+virtualbox           \ 
+mumble               \ 
+dolphin              \ 
+bashtop              \
+resolvconf           \
+mozillavpn           \
+ssh                  \
+xclip                \
+vlc                  \
+obs-studio           \
+vscode               \
+gimp                 \
+
+
+
 sudo apt install snapd
 snap install discord
 snap install spotify
