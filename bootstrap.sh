6347db6a81e574631c7b63bc7c8c49e8482891bc
dull-kiwi <lakb2f1h@anonaddy.com>
2024-04-07 16:19:59 -0400

    merged ubuntu and fedora into one file. added support for tmux/vim sync

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,72 @@
-sudo add-apt-repository ppa:mozillacorp/mozillavpn
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y  \
-vim                  \
-steam                \
-python3              \
-python3-pip          \
-git                  \
-git-gui              \
-openssh-server       \
-virtualbox           \
-mumble               \
-dolphin              \
-bashtop              \
-resolvconf           \
-mozillavpn           \
-ssh                  \
-xclip                \
-vlc                  \
-obs-studio           \
-gimp                 \
-net-tools            \
-gnome-shell-extension-manager\
-cmake\
-tmux
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+FLAG=$1
+OS=$(uname -n)
+echo $OS
 
-sudo apt install snapd
-snap install discord
-snap install spotify
-snap install mc-installer
-snap install code --classic
+if [[ $OS != "fedora" && $OS != "ubuntu" ]]; then
+    echo "$OS not supported"
+fi
 
-echo "alias xc='xclip -sel c'" >> ~/.bashrc
+git submodule update --init --recursive
 
+installPackages() {
+    if [[ $OS == "fedora" ]]; then
+        sudo dnf upgrade --refresh
+        sudo dnf install -y \
+            vim \
+            python3 \
+            python3-pip \
+            python3-devel \
+            git-gui \
+            openssh-server \
+            htop \
+            vlc \
+            net-tools \
+            tmux \
+            cmake \
+            gcc \
+            g++ \
+            make
+    elif [[ $OS == "ubuntu" ]]; then
+        sudo apt update && sudo apt upgrade -y
+        sudo apt install -y \
+            vim \
+            python3 \
+            python3-pip \
+            python3-devel \ 
+            git-gui \
+            openssh-server \
+            htop \
+            vlc \
+            net-tools \
+            tmux \
+            make \
+            cmake \ 
+            cmake3 \
+            build-essentials
+    fi
+}
+
+configureVim() {
+    rm -rf ~/.vim ~/.vimrc
+    ln -s $SCRIPT_DIR/linux_env/.vim ~/.vim
+    ln -s $SCRIPT_DIR/linux_env/.vimrc ~/.vimrc
+    cd $SCRIPT_DIR/linux_env/.vim/bundle/YouCompleteMe
+    python3 install.py --clangd-completer
+}
+
+configureTmux() {
+    rm ~/.tmux.conf
+    ln -s $SCRIPT_DIR/linux_env/.tmux.conf ~/.tmux.conf
+}
+
+if [[ $FLAG == "vim" ]]; then
+    configureVim
+elif [[ $FLAG == "tmux" ]]; then
+    conifgureTmux
+elif [[ $FLAG == "all" ]]; then
+    installPackages
+    configureVim
+    configureTmux 
+fi
